9114d81b7bc708d0e63f5cae04d57c2af9f51f6d
Michael Ruhnke <michael@marble.io>
2018-10-24 18:25:58 -0700

    compile with 4 threads

--- build_package.sh
+++ build_package.sh
@@ -6,6 +6,7 @@ if [ ! -d "build" ]; then
   cp description-pak build/
 fi
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DG2O_USE_OPENMP=1 -j4 ../
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DG2O_USE_OPENMP=1 ../
+make -j4
 sudo apt-get update && sudo apt-get install checkinstall
 sudo checkinstall --pkgversion="1.0.0" --pkglicense="BSD,LGPL,GPL" --maintainer="michael@marble.io" --requires="libeigen3-dev,libsuitesparse-dev,libqglviewer-dev" --pkgname=libg2o-dev --conflicts="ros-kinetic-libg2o"
