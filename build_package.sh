01712c3b54966eec75aeea5f527476d4d867b719
Michael Ruhnke <michael@marble.io>
2018-10-24 18:24:08 -0700

    added default description

--- build_package.sh
+++ build_package.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 git apply debian-package.patch
+
+if [ ! -d "build" ]; then
+  mkdir build
+  cp description-pak build/
+fi
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DG2O_USE_OPENMP=1 -j4 ../
-sudo checkinstall --pkglicense="BSD,LGPL,GPL" --maintainer="michael@marble.io" --requires="libeigen3-dev,libsuitesparse-dev,libqglviewer-dev" --pkgname=libg2o-dev --conflicts="ros-kinetic-libg2o"
+sudo apt-get update && sudo apt-get install checkinstall
+sudo checkinstall --pkgversion="1.0.0" --pkglicense="BSD,LGPL,GPL" --maintainer="michael@marble.io" --requires="libeigen3-dev,libsuitesparse-dev,libqglviewer-dev" --pkgname=libg2o-dev --conflicts="ros-kinetic-libg2o"
